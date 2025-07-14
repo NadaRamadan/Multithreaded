2d562bd799f6606476d404338524651d8ad5ca89
NadaRamadan <nada7.ra@gmail.com>
2025-07-14 18:40:35 +0300

    added threads

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Multithreaded_file_processor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           var folderPath= @"\text files";
+            string[] files = Directory.GetFiles(folderPath, "*.txt");
+
+            foreach (string file in files)
+            {
+                Thread t = new Thread(() => Proccess_file(file));
+                t.Start();
+
+
+
+            }
+        }
+        void Proccess_file(string filePath)
+        {
+
+        }
+
+    }
+}
